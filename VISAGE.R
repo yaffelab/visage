960caaeaa87afd7124bd19578c254697b54c5e6c
Brian Joughin <bjoughin@Brians-MacBook-Pro-4.local>
2019-07-16 09:22:41 -0400

    Added VISAGE reference to code and documentation.

--- VISAGE.R
+++ VISAGE.R
@@ -11,8 +11,8 @@
 # VISAGE Reveals a Targetable Mitotic Spindle Vulnerability in Cancer Cells.
 # Patterson JC, Joughin BA, Prota AE, Mühlethaler T, Jonas OH, Whitman MA, Varmeh S, Chen S, 
 # Balk SP, Steinmetz MO, Lauffenburger DA, Yaffe MB.
-# Cell Syst. 2019 
-# PMID: XXXXXXXX
+# Cell Syst. 2019 Jun 26. pii: S2405-4712(19)30190-5. doi: 10.1016/j.cels.2019.05.009.
+# PMID: 31302152
 
 
 # Install and load necessary packages.
